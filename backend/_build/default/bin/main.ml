0d343bfbc0fec266ebc45e80f9f07b10dc6d0647
Vibilan Jayanth <vibilan@firstindo.com>
2023-06-14 22:12:04 +0800

    added backend gitignore

--- backend/_build/default/bin/main.ml
+++ backend/_build/default/bin/main.ml
@@ -1,1 +1,12 @@
-let () = print_endline "Hello, Jerri!"
+(* let () = print_endline "Hello, Jerri!" *)
+
+let json_string = {|
+  {"number" : 42,
+   "string" : "yes",
+   "list": ["for", "sure", 42]}|}
+(* val json_string : string *)
+
+let json = Yojson.Safe.from_string json_string
+(* val json : Yojson.Safe.t *)
+
+let () = Format.printf "Parsed to %a" Yojson.Safe.pp json
