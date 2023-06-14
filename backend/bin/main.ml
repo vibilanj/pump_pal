a7c3cb002c807fac5fbf090ce0173b2d0d05e803
Vibilan Jayanth <vibilan@firstindo.com>
2023-06-14 23:17:34 +0800

    trying dune project

--- backend/bin/main.ml
+++ backend/bin/main.ml
@@ -1,12 +1,2 @@
-(* let () = print_endline "Hello, Jerri!" *)
-
-let json_string = {|
-  {"number" : 42,
-   "string" : "yes",
-   "list": ["for", "sure", 42]}|}
-(* val json_string : string *)
-
-let json = Yojson.Safe.from_string json_string
-(* val json : Yojson.Safe.t *)
-
-let () = Format.printf "Parsed to %a" Yojson.Safe.pp json
+let () = print_endline "Hello, Jerri!"
+let () = print_endline (Backend.Test.t2_str 3)
---
+++ backend/lib/test.ml
@@ -0,0 +1,1 @@
+let t2_str a = string_of_int (a * 2);;
