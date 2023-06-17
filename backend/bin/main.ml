d550f6ee9b059766c4c433f48030aff8bdeca50a
Vibilan Jayanth <vibilan@firstindo.com>
2023-06-17 15:18:37 +0800

    promise example

--- backend/bin/main.ml
+++ backend/bin/main.ml
@@ -1,14 +1,29 @@
-let count = ref 0
+let successful = ref 0
+let failed = ref 0
 
 let count_requests (inner_handler: Dream.handler) (request: Dream.request) : Dream.response Lwt.t =
-  count := !count + 1;
-  inner_handler request
+  try%lwt 
+    let%lwt response = inner_handler request in 
+    successful := !successful + 1;
+    Lwt.return response
+  
+  with exn ->
+    failed := !failed + 1;
+    raise exn
+
+
 
 let () =
   Dream.run 
   @@ Dream.logger
   @@ count_requests
   @@ Dream.router [
-         Dream.get "/" (fun _ 
-          -> Dream.html (Printf.sprintf "Saw %i request(s)!" !count));
-       ]
+    Dream.get "/fail" 
+      (fun _ -> 
+        raise (Failure "The Web app failed!"));
+
+    Dream.get "/" (fun _ -> 
+      Dream.html (Printf.sprintf
+        "%3i requests(s) successful<br>%3i request(s) failed"
+        !successful !failed));
+  ]
