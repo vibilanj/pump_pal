6de821a64021fad0bba2667bbb136d806f7b928c
Vibilan Jayanth <vibilan@firstindo.com>
2023-06-17 15:10:52 +0800

    counter example

--- backend/bin/main.ml
+++ backend/bin/main.ml
@@ -1,4 +1,14 @@
-let () = 
-  Dream.run
-  @@ Dream.logger 
-  @@ fun _ -> Dream.html "Good morning, world!"
+let count = ref 0
+
+let count_requests (inner_handler: Dream.handler) (request: Dream.request) : Dream.response Lwt.t =
+  count := !count + 1;
+  inner_handler request
+
+let () =
+  Dream.run 
+  @@ Dream.logger
+  @@ count_requests
+  @@ Dream.router [
+         Dream.get "/" (fun _ 
+          -> Dream.html (Printf.sprintf "Saw %i request(s)!" !count));
+       ]
